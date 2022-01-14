07c321daccb8854af2f47f4d8c5b7bf8c6d1d907
vinylrich <whktjd0109@gmail.com>
2022-01-14 13:06:44 +0900

    regame feat

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ function afterCount(){
                 alert("you entered " + enter.innerText + "!")
                 entercount=0
                 enter.innerText="Enter me!"
+                enter.classList.add("hidden")
                 return;
             }
             countdown--;
@@ -45,11 +46,6 @@ function beforeCount(){
     })
 
 }
-async function delay(ms) {
-    // return await for better async stack trace support in case of errors.
-    return await new Promise(resolve => setTimeout(resolve, ms));
-}
-
 
 function clickBtnHandler(){
    countDownFunc()
