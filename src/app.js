4780ce12f9132cd55e7320fed48e2575a66667cd
vinylrich <whktjd0109@gmail.com>
2022-01-14 11:03:31 +0900

    end!

--- src/app.js
+++ src/app.js
@@ -7,38 +7,50 @@ const modalbtn = document.getElementById("modal_btn")
 const closeBtn = modal.querySelector(".close-area")
 let entercount = 0
 
-function asd(callback){
-    beforeCount();
+async function countDownFunc(){
+    await beforeCount();
+    afterCount()
+
 }
 function afterCount(){
-    const aftercounter = setInterval(function(){
         let countdown=10;
-        console.log(countdown)
-        h2.innerText=String(countdown);
-        if (countdown<=0){
-            clearInterval(aftercounter);
-            return;
-        }
-        countdown--;
-    },1000);
+        const aftercounter = setInterval(function(){
+            console.log(countdown)
+            h2.innerText=String(countdown);
+            if (countdown <= 0){
+                clearInterval(aftercounter);
+                alert("you entered " + enter.innerText + "!")
+                return;
+            }
+            countdown--;
+        },1000);
 }
 function beforeCount(){
-    let countdown = 5
-    const beforecounter = setInterval(function(){
-        h2.classList.remove(HIDDEN_CLASSNAME);
-        h2.innerText=String(countdown)
-        if (countdown <= 0) {
-            clearInterval(beforecounter);
-            h2.innerText="Start!"
-            enter.classList.remove(HIDDEN_CLASSNAME)
-            return;
-        }
-        countdown--;
-    },1000);
+    return new Promise(function (resolve){
+        let countdown = 5
+        const beforecounter = setInterval(function(){
+            h2.classList.remove(HIDDEN_CLASSNAME);
+            h2.innerText=String(countdown)
+            if (countdown <= 0) {
+                clearInterval(beforecounter);
+                h2.innerText="Start!"
+                enter.classList.remove(HIDDEN_CLASSNAME)
+                resolve();
+                return;
+            }
+            countdown--;
+        },1000);
+    })
+
+}
+async function delay(ms) {
+    // return await for better async stack trace support in case of errors.
+    return await new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
 function clickBtnHandler(){
-   asd(afterCount);
+   countDownFunc()
 }
 
 function mouseenterHandler(){
